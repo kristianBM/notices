e1a922a2a6198ebda7f79c1fb2366dfe1f43e0a5
Kristian Mello <kristian.bonfim@hotmail.com>
2022-01-20 15:33:55 -0300

    Author Crud implementd

--- backend/src/main/java/com/surfersolution/notices/resources/AuthorResource.java
+++ backend/src/main/java/com/surfersolution/notices/resources/AuthorResource.java
@@ -1,10 +1,62 @@
 package com.surfersolution.notices.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.surfersolution.notices.domain.Author;
+import com.surfersolution.notices.services.AuthorService;
 
 @RestController
 @RequestMapping(value = "/api/v1/authors")
 public class AuthorResource {
 
+	@Autowired
+	private AuthorService service;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Author>> findAll() {
+		List<Author> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Optional<Author>> findById(@PathVariable Long id) {
+		Optional<Author> obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+
+	}
+	
+	@Transactional
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Author> insert(@RequestBody Author obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@Transactional
+	@RequestMapping(method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@Transactional
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Author> update (@PathVariable Long id, @RequestBody Author obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
 }
